5715f7d39ead346448d48ed6d55a3d16e022b657
Caio Dias <77282689+CaioLDias@users.noreply.github.com>
2024-01-23 11:39:06 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import plotly.express as px
+#import plotly.express as px
 from dataset import df
 from utils import format_number
 from graphics import grafico_map_estado, grafico_rec_mensal, grafico_rec_estado, grafico_rec_categoria, grafico_rec_vendedores, grafico_venda_vendedores
